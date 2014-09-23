e129711d899609855a70f335a75f57688d209770
salamandermike <salamandermike@gmail.com>
2014-09-22 21:47:14 -0700

    Angular route switched on and working

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Rails.application.routes.draw do
-  # root to: "site#index"
+  root to: "site#index"
 
-  get "/" => "session#new"
-  post "/" => "session#create"
-  post "/users/:user_id/events/new" => "events#create"
-  get "/login" => "session#new"
-  get "/logout" => "session#destroy"
+  # get "/" => "session#new"
+  # post "/" => "session#create"
+  # post "/users/:user_id/events/new" => "events#create"
+  # get "/login" => "session#new"
+  # get "/logout" => "session#destroy"
 
-  root to: 'session#new'
-
+  # root to: 'session#new'
+  resources :site
   resources :session
   resources :gifter_templates
   resources :users do
     resources :events
     resources :profile
   end
-  # match "*path", to: "site#index", via: "get"
+  match "*path", to: "site#index", via: "get"
 end
